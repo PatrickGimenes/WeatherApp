e73d64d7268221139ba8ebab6aa232dd6f64c0ed
LucasAlmeida-jpg <lucasalmeidagouvea123@gmail.com>
2023-10-07 12:05:24 -0300

    modal create

--- src/App.vue
+++ src/App.vue
@@ -3,7 +3,7 @@
     <div class="w-full h-min flex items-center justify-between">
       <font-awesome-icon icon="fa-solid fa-location-dot" class="absolute text-slate-900 text-2xl pl-1" />
       <input v-model="city"
-        class="text-slate-900 w-4/5 uppercase text-2xl font-medium pl-8 rounded-md placeholder:text-xl placeholder:capitalize"
+        class="text-slate-900 w-4/5 text-2xl font-medium pl-8 rounded-md placeholder:text-xl placeholder:capitalize"
         type="text" placeholder="Digite sua localização" />
       <button @click="fetchData" class="cursor-pointer w-10 h-10 bg-slate-300 rounded-xl text-lg hover:bg-slate-900 ">
         <font-awesome-icon icon="fa-solid fa-magnifying-glass" />
@@ -19,59 +19,104 @@
         <p class="text-slate-900">Humidade: {{ humidity }} %</p>
       </div>
       <div class="flex items-center w-1/2 h-24">
-        <font-awesome-icon icon="fa-solid fa-wind" class="pr-4" />
-        <p class="text-slate-900">Vento: {{ wind }} Km/h</p>
+        <div>
+          <font-awesome-icon icon="fa-solid fa-wind" class="pr-4" />
+        </div>
+        <div>
+          <p class="text-slate-900">Vento: {{ wind }} Km/h</p>
+        </div>
+      </div>
+    </div>
+  </div>
+  <div v-if="errorModal" class="modal">
+    <div class="modal-content">
+      <div>
+        <p>{{ errorMessage }}</p>
+      </div>
+      <div>
+        <span class="close" @click="errorModal = false">&times;</span>
       </div>
     </div>
   </div>
 </template>
 
 
-<script >
-import axios from 'axios'
+<script>
+import axios from 'axios';
 
 export default {
   data() {
     return {
-      city: null,
-      temperature: '0',
-      weather: 'Digite a cidade',
-      humidity: '0',
-      wind: '0',
-      api: ''
-    }
+      city: '',
+      temperature: 0,
+      weather: 'Desconhecido',
+      humidity: 0,
+      wind: 0,
+      api: '',
+      errorModal: false,
+      errorMessage: '',
+    };
   },
   methods: {
+    async fetchData() {
+      try {
+        const response = await axios.get(`https://api.openweathermap.org/data/2.5/weather?q=${this.city}&units=metric&appid=${this.api}`);
+        const weatherData = response.data;
+        const description = weatherData.weather[0].description;
+
+        this.city = weatherData.name;
+        this.temperature = weatherData.main.temp;
+        this.weather = this.translateWeatherDescription(description);
+        this.humidity = weatherData.main.humidity;
+        this.wind = weatherData.wind.speed;
+      } catch (error) {
+        this.errorMessage = 'Erro ao buscar dados meteorológicos';
+        this.errorModal = true;
+      }
+    },
+
     translateWeatherDescription(description) {
       const translations = {
-        'clear sky': 'céu limpo',
-        'few clouds': 'poucas nuvens',
-        'scattered clouds': 'nuvens dispersas',
-        'broken clouds': 'nuvens quebradas',
-        'overcast clouds': 'nuvens encobertas',
-        'light rain': 'chuvinha fraquinha',
-        'moderate rain': 'chuvinha moderada',
-        'heavy rain': 'chuva intensa',
+        'clear sky': 'Céu limpo',
+        'few clouds': 'Poucas nuvens',
+        'scattered clouds': 'Nuvens dispersas',
+        'broken clouds': 'Nuvens quebradas',
+        'overcast clouds': 'Muita nuvem',
+        'light rain': 'Chuva fraca',
+        'moderate rain': 'Chuva moderada',
+        'heavy rain': 'Chuva intensa',
       };
-
       return translations[description] || description;
     },
-
-    fetchData() {
-      axios.get(`https://api.openweathermap.org/data/2.5/weather?q=${this.city}&units=metric&appid=${this.api}`)
-        .then((response) => {
-          const weatherData = response.data;
-          this.temperature = weatherData.main.temp;
-          this.weather = this.translateWeatherDescription(weatherData.weather[0].description);
-          this.humidity = weatherData.main.humidity;
-          this.wind = weatherData.wind.speed;
-        })
-    },
   },
-
-  mounted() {
-    this.fetchData();
+  async mounted() {
+    await this.fetchData();
   },
 };
 </script>
+<style>
+.modal {
+  background: red;
+  color: white;
+  text-align: center;
+  border-radius: 12px;
+  margin-top: 12px;
+  padding: 10px;
+}
 
+.modal-content {
+  display: flex;
+  align-items: center;
+  justify-content: space-between;
+}
+
+.close {
+  cursor: pointer;
+  background: white;
+  color: red;
+  border-radius: 50%;
+  padding: 2px 8px;
+  display: flex;
+  align-items: center;
+}
+</style>
